ce4743d65afdd66e693d5be4f55b7900aacf4cda
angelajazz <ajggutierrez28@gmail.com>
2022-09-12 12:41:33 +0800

    sorted css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,13 +4,13 @@ import Results from "./Results";
 import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("props.defaultKeyword");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
   let [photos, setPhotos] = useState(null);
 
-  function handleDictionaryResponse(response) {
+  function handleResponse(response) {
     setResults(response.data[0]);
   }
 
@@ -20,13 +20,13 @@ export default function Dictionary() {
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleDictionaryResponse);
+    axios.get(apiUrl).then(handleResponse);
 
-    let pexelsApiKey =
+    const pexelsApiKey =
       "563492ad6f917000010000011240b278c17e441f88c9a466efa11e5a";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
@@ -49,10 +49,14 @@ export default function Dictionary() {
       <div className="Dictionary">
         <section>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
+            <input
+              type="search"
+              placeholder="search"
+              onChange={handleKeywordChange}
+            />
           </form>
           <div className="hint">
-            suggest words: sunset, wine, yoga, plant...
+            Suggested Words: sunset, wine, yoga, plant...
           </div>
         </section>
         <Results results={results} />
@@ -61,6 +65,6 @@ export default function Dictionary() {
     );
   } else {
     load();
-    return "Loading";
+    return "loading...";
   }
 }
